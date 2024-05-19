b2222dd3182b2ccafb6d4886d5043ef533e30bdd
yusufferdogan <yusuffx03@gmail.com>
2024-05-19 23:13:33 +0300

    create Todo Entity

---
+++ src/main/java/com/example/springboottodo/Entity/Todo.java
@@ -0,0 +1,29 @@
+package com.example.springboottodo.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "is_done")
+    private boolean done;
+
+    @Column(name = "due_date")
+    private String dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users user;
+}
